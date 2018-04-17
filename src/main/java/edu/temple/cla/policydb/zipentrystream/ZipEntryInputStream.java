57a73acfde7fe96cf3b23b3b6953b49682485ade
Paul Wolfgang <wolfgang@temple.edu>
2018-04-17 16:30:49 -0400

    Added documentation and modified pom to generate javadoc.

--- src/main/java/edu/temple/cla/policydb/zipentrystream/ZipEntryInputStream.java
+++ src/main/java/edu/temple/cla/policydb/zipentrystream/ZipEntryInputStream.java
@@ -38,8 +38,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipInputStream;
 
+/**
+ * This is a wrapper for a ZipInputStream that allows an entry to be treated
+ * as an InputStream.
+ * @author Paul Wolfgang
+ */
 public class ZipEntryInputStream extends InputStream {
 
+    /**
+     * Method to determine if a file is Zip File.  It examines the first two
+     * bytes to see if they are the characters "PK".
+     * @param file The file to be tested.
+     * @return True if the file is a zip file.
+     * @throws IOException 
+     */
     public static boolean isZipFile(File file) throws IOException {
         boolean isIt;
         try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
@@ -48,6 +60,13 @@ public class ZipEntryInputStream extends InputStream {
         return isIt;
     }
 
+    /**
+     * Method to determine if an input stream is a Zip file. It examines the
+     * first two bytes to see if they are the characters "PK"
+     * @param inputStream A buffered input stream to be examined.
+     * @return True if the stream is a zip file.
+     * @throws IOException 
+     */
     public static boolean isZipEntry(final BufferedInputStream inputStream) throws IOException {
         byte[] firstTwoBytes = new byte[2];
         inputStream.mark(2);
@@ -57,17 +76,31 @@ public class ZipEntryInputStream extends InputStream {
         return isIt;
     }
     
+    /** The wrapped ZipInputStream */
     private final ZipInputStream zipInputStream;
     
+    /** 
+     * Constructor.
+     * @param zipInputStream The stream to be wrapped by this class.
+     */
     public ZipEntryInputStream(ZipInputStream zipInputStream) {
         this.zipInputStream = zipInputStream;
     }
     
+    /**
+     * Read the next byte. Calls read on the wrapped input stream.
+     * @return The next byte as an int, or -1 if EOF. 
+     * @throws IOException 
+     */
     @Override
     public int read() throws IOException {
         return zipInputStream.read();
     }
     
+    /**
+     * Close the entry. Calls closeEntry on the wrapped ZipInputStream.
+     * @throws IOException 
+     */
     @Override
     public void close() throws IOException {
         zipInputStream.closeEntry();
